0a8a2cb730858fbfd1e4c9cb1ca79d0a7c559c10
Guilherme de Sá Gonçalves <92813703+GuilhermeDSa1013@users.noreply.github.com>
2023-04-30 15:15:33 -0300

    Update cubo.java

--- JavaProjects/Teste/src/cubo.java
+++ JavaProjects/Teste/src/cubo.java
@@ -1,5 +1,3 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class cubo {
@@ -9,20 +7,17 @@ public class cubo {
         Scanner scanner = new Scanner(System.in);
         Integer A = scanner.nextInt();
         Integer B = scanner.nextInt();
-        
-        //Criando array do intervalo
-        ArrayList<Integer> intervalo = new ArrayList<Integer>();
 
         //Variável que conterá a quantidade de números que tem raízes exatas
         Integer saida = 0;
 
-        //Algoritmo para adicionar no ArrayList intervalo
+        //Algoritmo para conferir se determinado número tem raízes exatas
         for (int i = A; i <= B; i++){ //Começa do limite inferior e vai incrementando 1 até chegar no limite superior
-            intervalo.add(i); //Adiciona na lista
 
-            //Confere se o resto da divisão entre a raíz quadrada por 1, é zero, se sim, é exata e calcula a raíz cúbica
+            //Confere se o resto da divisão entre a raíz quadrada por 1, é zero, se sim, é inteiro e calcula a raíz cúbica
             if (Math.sqrt(i) % 1 == 0){ 
-                //Confere se a raíz cúbica é exata, se sim, incrementa a saída
+
+                //Confere se a raíz cúbica é inteira, se sim, incrementa a saída
                 if  (Math.cbrt(i) % 1 == 0){
                     saida++;
                 }
@@ -33,7 +28,5 @@ public class cubo {
         System.out.println(saida);
 
         scanner.close();
-    }
-
-    
+    } 
 }
